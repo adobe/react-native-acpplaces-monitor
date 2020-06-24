a0d6ea6c291a5f81278d1ce9f0b7dfd0b07d26ab
ravinsingh jain <ravjain@adobe.com>
2020-06-24 10:26:46 -0700

    Updating master

--- sample/ACPPlacesMonitorSample/ios/ACPPlacesMonitorSample/AppDelegate.m
+++ sample/ACPPlacesMonitorSample/ios/ACPPlacesMonitorSample/AppDelegate.m
@@ -1,91 +1,61 @@
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+  NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe. (See LICENSE-MIT for details)
+  */
+
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
-#import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-#import <RCTACPCore/ACPCore.h>
-#import <RCTACPCore/ACPLifecycle.h>
-#import <RCTACPCore/ACPIdentity.h>
-#import <RCTACPCore/ACPSignal.h>
-#import <RCTACPPlaces/ACPPlaces.h>
-#import <RCTACPPlacesMonitor/ACPPlacesMonitor.h>
+  #import <React/RCTBundleURLProvider.h>
+  #import <React/RCTRootView.h>
+  #import <RCTACPCore/ACPCore.h>
+  #import <RCTACPCore/ACPLifecycle.h>
+  #import <RCTACPCore/ACPIdentity.h>
+  #import <RCTACPCore/ACPSignal.h>
+  #import <RCTACPPlaces/ACPPlaces.h>
+  #import <RCTACPPlacesMonitor/ACPPlacesMonitor.h>
 
-#if DEBUG
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
-}
-#endif
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+  - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
-  InitializeFlipper(application);
-#endif
-
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"ACPPlacesMonitorSample"
-                                            initialProperties:nil];
+RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+  moduleName:@"ACPPlacesSample"
+  initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  
-  [ACPCore setLogLevel:ACPMobileLogLevelVerbose];
-  [ACPCore configureWithAppId:@"launch-EN81be7cedb7f14132968641a6ec683adf"];
-  [ACPCore setWrapperType:ACPMobileWrapperTypeReactNative];
-  [ACPIdentity registerExtension];
-  [ACPLifecycle registerExtension];
-  [ACPSignal registerExtension];
-  [ACPPlaces registerExtension];
-  [ACPPlacesMonitor registerExtension];
-  
-  const UIApplicationState appState = application.applicationState;
-  [ACPCore start:^{
-    // only start lifecycle if the application is not in the background
-    if (appState != UIApplicationStateBackground) {
-      [ACPCore lifecycleStart:nil];
-    }
-  }];
-  
-  return YES;
-}
+self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+UIViewController *rootViewController = [UIViewController new];
+rootViewController.view = rootView;
+self.window.rootViewController = rootViewController;
+[self.window makeKeyAndVisible];
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  [ACPCore lifecyclePause];
-}
+[ACPCore setLogLevel:ACPMobileLogLevelVerbose];
+[ACPCore configureWithAppId:@"yourAppId"];
+[ACPCore setWrapperType:ACPMobileWrapperTypeReactNative];
+[ACPIdentity registerExtension];
+[ACPLifecycle registerExtension];
+[ACPSignal registerExtension];
+[ACPPlaces registerExtension];
+[ACPPlacesMonitor registerExtension];
 
+[ACPCore start:nil];
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-  // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  [ACPCore lifecycleStart:nil];
+return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  {
+    #if DEBUG
+      return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
